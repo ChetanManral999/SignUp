3070e6306833ffb8c4a165bfdbe5c0b3e561607a
Chetan Manral <137912484+ChetanManral999@users.noreply.github.com>
2023-07-26 00:43:11 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,45 @@
+package com.uniquedeveloper.registeration;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uname=request.getParameter("username");
+		String upwd=request.getParameter("password");
+		Connection con=null;
+		HttpSession session=request.getSession();
+		RequestDispatcher dispatcher=null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/youtube?useSSL=false","root",String.valueOf(2119));
+			PreparedStatement pst=con.prepareStatement("select * from users where uname=? and upwd=?");
+			pst.setString(1, uname);
+			pst.setString(2, upwd);
+			ResultSet rs=pst.executeQuery();
+			if(rs.next()) {
+				session.setAttribute("name", rs.getString("uname"));
+				dispatcher=request.getRequestDispatcher("index.jsp");
+			}
+			else {
+				request.setAttribute("status", "failed");
+				dispatcher=request.getRequestDispatcher("login.jsp");	
+			}
+			dispatcher.forward(request, response);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
